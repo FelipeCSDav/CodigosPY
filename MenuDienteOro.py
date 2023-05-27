79ef214606503100af451e3a86ee09e6ca6d7d5b
FelipeCSDav <134576628+FelipeCSDav@users.noreply.github.com>
2023-05-27 13:45:59 -0400

    Update MenuDienteOro.py

--- MenuDienteOro.py
+++ MenuDienteOro.py
@@ -1,121 +1,128 @@
-opc=0
-opc2=0
-
-
-CP=250000
-ID=475000
-OB=800000
-
+menu1=0
+menu2=0
+total=0
+#Costos de Tratamientos
+Val_Car=250000
+Val_Imp=475000
+Val_Ort=800000
+#Descuentos de Trabajadores 
 TrAux=0.15
 TrAdm=0.10
 TrDoc=0.05
+#Contadores de Tratamientos
+ContCar=0
+ContImp=0
+ContOrt=0
 
-contcarillas=0
-contimplante=0
-contortodoncia=0
-
-total=0
-
-while opc!=3:
-    opc2=0
-    print("\nBienvenido a El Diente de Oro")
-    print("")
+#Menu 1
+while menu1!=3:
+    menu2=0
+    print("-"*35)
+    print("    Bienvenido a El Diente de Oro\n")
     print("1) Realizar cotizacion")
-    print("2) Reiniciar cotizacion   ")
-    print("3) Salir")
+    print("2) Reiniciar valores de cotizacion")
+    print("3) Cerrar cotizacion")
+    print("-"*35)
     while True:
         try:
-            opc=int(input("Ingrese la opcion: "))
-            if 1<= opc <= 3:
+            menu1=int(input("Ingrese una opcion: "))
+            if 1<=menu1<=3:
                 break
             else:
                 print("La opcion no esta dentro del rango\n")
         except:
-            print("Error de valor ingresado, intenta nuevamente\n")
-
-    if opc == 1:
-        while opc2!=4:
-            
-
-            print("\nLos tratamientos son\n")
-            print(f"1) Carillas Porcelana = ${CP}")
-            print(f"2) Implantes Dentales = ${ID}")
-            print(f"3) Ortodoncia Brackets = ${OB}")  
+            print("Error de valor ingresado, intenta nuevamente.\n")
+#Menu 2
+    if menu1==1:
+        while menu2!=4:
+            print("-"*35)
+            print("      Tratamientos Dentales\n")
+            print(f"1) Carillas Porcelana   =  ${Val_Car}")
+            print(f"2) Implantes Dentales   =  ${Val_Imp}")
+            print(f"3) Ortodoncia Brackets  =  ${Val_Ort}")  
             print("4) Terminar cotizacion")
+            print("-"*35)
             while True:
                 try:
-                    opc2=int(input("Ingresar eleccion: "))
-                    if 1<= opc2<=4:
+                    menu2=int(input("Ingresar tratamientos a comprar: "))
+                    if 1<=menu2<=4:
                         break
                     else:
                         print("La opcion no esta dentro del rango\n")
                 except:
-                    print("Error de valor ingresado, intenta nuevamente\n")
-            
-            if opc2==1:
-                contcarillas+=1
-                total+=CP
-            elif opc2==2:
-                contimplante+=1
-                total+=ID
-            elif opc2==3:
-                contortodoncia+=1
-                total+=OB
-
-            if opc2==4:
-                print("\nTipo de Trabajador\n")
-                print("1) Trabajador Auxiliar")
-                print("2) Trabajador Administrativo")
-                print("3) Trabajador Docente")
+                    print("Error de valor ingresado, intenta nuevamente.\n")
+            if 1<=menu2<=3:
                 while True:
                     try:
-                        tipo=int(input("Ingresar opcion: "))
-                        if 1 <= tipo <=3:
+                        cantidad=int(input("¿Cuantos desea cotizar?: "))
+                        if cantidad>=0:
+                            break
+                        else:
+                            print("La cantidad debe ser igual o mayor a 0")
+                    except:
+                        print("Error de valor ingresado, intenta nuevamente.\n")
+#Contadores y totales
+                if menu2==1:
+                    ContCar+=cantidad
+                    total+=Val_Car*ContCar
+                elif menu2==2:
+                    ContImp+=cantidad
+                    total+=Val_Imp*ContImp
+                elif menu2==3:
+                    ContOrt+=cantidad
+                    total+=Val_Ort*ContOrt
+#Menu (Tipo de Trabajador)
+            if menu2==4:
+                print("-"*50)
+                print("           Tipo de Trabajador\n")
+                print("1) Trabajador Auxiliar         Descuento: 15%")
+                print("2) Trabajador Administrativo   Descuento: 10%")
+                print("3) Trabajador Docente          Descuento: 5%")
+                print("-"*50)
+                while True:
+                    try:
+                        tipo=int(input("Ingresar el tipo de trabajador: "))
+                        if 1<=tipo<=3:
+                            print("Descuento aplicado")
                             break
                         else:
                             print("La opcion no esta dentro del rango\n")
                     except:
                         print("Error de valor ingresado, intenta nuevamente\n")
-                
-                
-                
+#Costo totla final con descuentos aplicados
                 if tipo==1:
                     totalfinal = total*(1-TrAux)
-                elif tipo== 2:
+                elif tipo==2:
                     totalfinal = total*(1-TrAdm)
-                elif tipo== 3:
+                elif tipo==3:
                     totalfinal = total*(1-TrDoc)
-                
-                print("Cotizacion")
+#Detalle de cotizacion
+                print("\n\n             *Detalle de cotizacion*")
                 print("-"*50)
-                if contcarillas>0:
-                    print(f"{contcarillas} Tratamiento Carillas Porcelana ${CP*contcarillas}")
-                if contimplante>0:
-                    print(f"{contimplante} Implantes Dentales ${ID*contimplante}")
-                if contortodoncia>0:
-                    print(f"{contortodoncia} Ortodoncia de Brackets ${OB*contortodoncia}")
-                print("-"*50)
-                print(f"SubTotal: {total}")
-                if tipo == 1:
-                    print(f"Descuento aplicado {TrAux*100}%   ${round(total-totalfinal)}")
-                elif tipo== 2:
-                    print(f"Descuento aplicado {TrAdm*100}%   ${round(total-totalfinal)}")
-                elif tipo== 3:
-                    print(f"Descuento aplicado {TrDoc*100}%   ${round(total-totalfinal)}")
+                if ContCar>0:
+                    print(f"{ContCar} Tratamiento Carillas Porcelana  ${Val_Car*ContCar}")
+                if ContImp > 0:
+                    print(f"{ContImp} Implantes Dentales              ${Val_Imp*ContImp}")
+                if ContOrt > 0:
+                    print(f"{ContOrt} Ortodoncia de Brackets          ${Val_Ort*ContOrt}")
+                if total> 0:
+                    print("-"*50)
+                print(f"SubTotal: ${total}")
+                if tipo == 1 :
+                    print(f"Descuento aplicado {round(TrAux*100)}%   -${round(total-totalfinal)}")
+                elif tipo == 2 :
+                    print(f"Descuento aplicado {round(TrAdm*100)}%   -${round(total-totalfinal)}")
+                elif tipo == 3 :
+                    print(f"Descuento aplicado {round(TrDoc*100)}%   -${round(total-totalfinal)}")
                 print("-"*50)
                 print(f"Total: ${round(totalfinal)}")
-                print("-"*50)
                 print(f"Son 12 cuotas de: ${round(totalfinal/12)}")
-                print("")
-                print("Sonria Bonito :)\n")
-                
-                
-    if opc==2:
+                print("-"*50)
+                print("Sonria Bonito :)\n\n\n")
+#Reinicio de valores de la cotizacion                         
+    if menu1==2:
         print("Valores reiniciados")
         total=0
-        contcarillas=0
-        contimplante=0
-        contortodoncia=0
-                
-
-    
+        ContCar=0
+        ContImp=0
+        ContOrt=0
